c3dcf6c87b7f4fff39abda5bb7e6806a6e7c21ea
Roy Ruiz <roy.ruiz@tuhh.de>
2020-05-27 01:33:00 +0200

    Finised python code to plot correctly based on desired step size. When running, create a virtual environment to import the right libraries

--- SiMCN-Ex04/rxPkOk_plot.py
+++ SiMCN-Ex04/rxPkOk_plot.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
 from matplotlib import style
 import math
+import decimal
+import plotly.express as px
 
 
 cols = ['Sim_T', 'Data']
@@ -17,35 +19,14 @@ min_time = float(math.floor(df['Sim_T'].min()))
 
 # time_arr = np.arange(min_time, max_time, 0.5)
 
-df_summary = df["Data"].groupby(pd.cut(df["Sim_T"], np.arange(min_time, max_time+0.2, 0.2))).sum()
+df_summary = df["Data"].groupby(pd.cut(df["Sim_T"], np.arange(
+    min_time, max_time+0.4, 0.4), labels=np.arange(int(min_time), int(max_time), decimal.Decimal('0.4')))).sum()
 
-print(df_summary)
-#print(max_time)
+with open('csv_data.csv', 'w') as csv_file:
+    df_summary.to_csv(path_or_buf=csv_file)
 
+df = pd.read_csv('csv_data.csv')
 
-'''y = np.ones((10, 1))
-y[0:5] = 2
-psum = y[0:5].sum(axis=0)
-print(y,"\n")
-print(psum)'''
-
-#for i in enumerate(time_arr):
-#  print(i)
-
-#user_visits = pd.DataFrame({'user_id': ['A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B'],
-#                            'visit_type': [1,  1,  3,  3,  3,  2,  2,  2,  2]})
-
-##print(user_visits)
-
-#User_Visits_summary = user_visits.groupby(
-#    ['user_id']).size().reset_index()
-
-#User_Visits_summary = User_Visits_summary.pivot(
-#    index='user_id', columns='visit_type', values=0)
-
-# Calculate total from sub-totals in new column
-#User_Visits_summary['Total_Visits'] = User_Visits_summary.sum(axis=1)
-
-#print(User_Visits_summary)
-
-
+fig = px.line(df, x='Sim_T', y='Data',
+              title='Throughput vs Time')
+fig.show()

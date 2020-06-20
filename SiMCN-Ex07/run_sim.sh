c8132f51b3a1d9853738aa3addb0d054f31b9d0c
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-06-20 19:07:57 +0200

    scripts to run simulations on the pool PCs

---
+++ SiMCN-Ex07/run_sim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN01 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN02 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN03 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN04 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN05 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN06 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN07 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN08 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN09 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN10 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL01 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL02 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL03 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL04 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL05 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL06 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL07 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL08 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL09 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL10 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini

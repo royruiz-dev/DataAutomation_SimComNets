866438418eff4eb36e71c5170f341ffb66bea7dd
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-06-22 04:03:55 +0200

    Improved the NED file, fixed VoIPApp cc file, and added configurations in the INI file to run simulations overnight

--- SiMCN-Ex07/VoIPApp.cc
+++ SiMCN-Ex07/VoIPApp.cc
@@ -1,12 +1,6 @@
-/*
- * VoIPApp.cc
- *
- *  Created on: Jun 18, 2020
- *      Author: royruiz
- */
-
 #include <omnetpp.h>
 #include "inet/applications/udpapp/UdpBasicApp.h"
+
 
 using namespace omnetpp;
 using namespace inet;
@@ -19,7 +13,6 @@ public:
     int numLostPackets;
     int numSentPackets;
     int numReceivedPackets;
-    simtime_t timePacketSent;
 
 protected:
     virtual void initialize(int stage) override;
@@ -41,39 +34,24 @@ void VoIPApp::initialize(int stage) {
 }
 
 void VoIPApp::sendPacket() {
-    //std::ostringstream payload;
-    //payload << "VoIPApp" << "-" << numSentPackets;
-
-    timePacketSent = simTime();
     UdpBasicApp::sendPacket();
-
-    //Packet *packet = new Packet(payload.str().c_str());
-    //Packet *packet = new cPacket(payload);
-    //const auto& packet = makeShared<ApplicationPacket>();
-    //packet->setChunkLength(B(par("messageLength")));
-
-    //L3Address destAddr = chooseDestAddr();
-    //emit(packetSentSignal, packet);
-    //socket.sendTo(packet, destAddr, destPort);
-
     numSentPackets++;
 }
 
 void VoIPApp::processPacket(Packet *pk) {
     simtime_t timePacketReceived = simTime();
-    simtime_t duration = timePacketReceived - timePacketSent;
-
+    simtime_t duration = timePacketReceived - pk->getCreationTime();
     emit(packetReceivedSignal, pk);
-    //EV_INFO << "Received packet: " << UdpSocket::getReceivedPacketInfo(pk) << endl;
-    delete pk;
-    if (duration > acceptableDelay) {
+    if (duration.dbl() > acceptableDelay) {
         numLostPackets++;
+        EV << "Lost Packet hahahahas: " << numLostPackets;
     }
     numReceivedPackets++;
+    delete pk;
 }
 
 void VoIPApp::finish() {
-    packetLossRate = ((double)numLostPackets) / ((double)numSentPackets);
+    double packetLossRate = ((double)numLostPackets) / ((double)numReceivedPackets);
     recordScalar("Average Loss Rate", packetLossRate);
     recordScalar("Packets Lost", numLostPackets);
     recordScalar("Packets Sent", numSentPackets);
--- SiMCN-Ex07/run_sim.sh
+++ SiMCN-Ex07/run_sim.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN01 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN02 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN03 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN04 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN05 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN06 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN07 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN08 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN09 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN10 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL01 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL02 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL03 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL04 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL05 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL06 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL07 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL08 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL09 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL10 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN01_for_Internet -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN02_for_Internet -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN03_for_Internet -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN_for_VoIP_and_Internet_Best_Case -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL_for_VoIP_and_Internet_Best_Case -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL01_for_Internet -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL02_for_Internet -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL03_for_Internet -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c ISDN_for_VoIP_and_Internet_Worse_Case -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c DSL_for_VoIP_and_Internet_Worse_Case -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini

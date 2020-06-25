4d34688b20871963f4588128bac657fd53b80eb3
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-06-25 22:30:07 +0200

    scripts

--- SiMCN-Ex07/scave_tool_VoIP_DSL.sh
+++ SiMCN-Ex07/scave_tool_VoIP_DSL.sh
@@ -4,3 +4,6 @@ scavetool export  -f 'module(*.user[*].pppg$o[0].channel) AND name(throughput:la
 scavetool export  -f 'module(*.user[*].pppg$o[0].channel) AND name(throughput:last)' -o results/NO_VOIP_DSL_rpt10_scenario_02_throughput.csv -F CSV-S results/NO_VOIP_DSL_rpt10_scenario_02-*.sca
 scavetool export  -f 'module(*.user[*].pppg$o[0].channel) AND name(throughput:last)' -o results/NO_VOIP_DSL_rpt30_scenario_02_throughput.csv -F CSV-S results/NO_VOIP_DSL_rpt30_scenario_02-*.sca
 
+
+
+

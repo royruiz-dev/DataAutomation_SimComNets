5ad7a31f265ccfa44627111386e5c0a33f8c661e
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-06-25 03:08:05 +0200

    scripts and .ini file

--- SiMCN-Ex07/run_sim01.sh
+++ SiMCN-Ex07/run_sim01.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_DSL_rpt03_scenario_02 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_DSL_rpt10_scenario_02 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_DSL_rpt30_scenario_02 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_DSL_rpt03_scenario_02 -r 0..9 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_DSL_rpt10_scenario_02 -r 0..29 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_DSL_rpt30_scenario_02 -r 0..89 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini

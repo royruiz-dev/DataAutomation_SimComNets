2a01a0ca41f13c5ee8b714a03c39a8acce6d6fc6
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-06-25 02:38:23 +0200

    scripts and .ini file

--- SiMCN-Ex07/run_sim.sh
+++ SiMCN-Ex07/run_sim.sh
@@ -2,3 +2,5 @@
 opp_runall -j$(nproc) -b8 ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_ISDN_rpt30_scenario_01 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
 opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_ISDN_rpt03_scenario_01 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
 opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c NO_VOIP_ISDN_rpt10_scenario_01 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+
+

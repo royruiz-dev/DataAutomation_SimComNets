1b675333eab5dcb329b9c9e032a013b1fc39a0ad
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-07-03 17:57:47 +0200

    updated scripts

--- SiMCN-Ex07/scave_tool_VoIP_DSL.sh
+++ SiMCN-Ex07/scave_tool_VoIP_DSL.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_300_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_300-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_300_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_300_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_300_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_300-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_150-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_150-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_5-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_5-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_310_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_310-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_310_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_310_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_310_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_310-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_250_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_250-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_250_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_250_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_250_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_250-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_150-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_150-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_5-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_5-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_310_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_310-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_310_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_310_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_310_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_310-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_250_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_250-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_250_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_250_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_250_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_250-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_150-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_150_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_150-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_5-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_5-*.sca

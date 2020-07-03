aec1787589c4d07dd3d6652c853ae2a185468a58
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-07-03 18:47:30 +0200

    updated scripts

--- SiMCN-Ex07/scave_tool_VoIP_DSL.sh
+++ SiMCN-Ex07/scave_tool_VoIP_DSL.sh
@@ -1,34 +1,67 @@
 #!/bin/bash
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_300.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_300-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_300.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_300_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_300_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_300-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_150.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_150-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_150.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_300_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_300_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_300-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_5.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_5-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_5.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_150_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_150_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_150-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_310.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_310-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_310.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_150_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_150_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_150-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_250.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_250-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_250.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_5_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_03_PAIRS_5_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_03_PAIRS_5-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_150.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_150-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_150.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_5_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_03_PAIRS_5_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_03_PAIRS_5-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_5.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_5-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_5.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_310_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_310_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_310-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_310.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_310-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_310.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_310_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_310_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_310-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_250.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_250-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_250.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_250_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_250_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_250-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_150.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_150-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_150.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_250_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_250_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_250-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_5.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_5-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_5.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_150_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_150_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_150-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_150_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_150_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_150-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_5_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_04_PAIRS_5_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_04_PAIRS_5-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_5_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_04_PAIRS_5_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_04_PAIRS_5-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_310_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_310_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_310-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_310_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_310-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_310_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_310-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_250_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_250_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_250-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_250_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_250-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_250_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_250-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_150_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_150_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_150-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_150_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_150-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_150_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_150-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_5_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_05_PAIRS_5_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_05_PAIRS_5-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_5_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_05_PAIRS_5_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_05_PAIRS_5-*.sca
--- SiMCN-Ex07/scave_tool_VoIP_ISDN.sh
+++ SiMCN-Ex07/scave_tool_VoIP_ISDN.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20_SENT.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20_RCVD.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_07_PAIRS.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_07_PAIRS_20-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_07_PAIRS.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_07_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20_SENT.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20_RCVD.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_ISDN_rpt03_scenario_07_PAIRS_SENT.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_07_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_ISDN_rpt03_scenario_07_PAIRS_RCVD.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_07_PAIRS_20-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_ISDN_rpt10_scenario_07_PAIRS_SENT.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_07_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_ISDN_rpt10_scenario_07_PAIRS_RCVD.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_07_PAIRS_20-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_10_MIXED.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_10_MIXED-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_10_MIXED.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_10_MIXED-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300_SENT.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300_RCVD.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300_SENT.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300_RCVD.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5_SENT.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5_RCVD.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5_SENT.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5_RCVD.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt03_scenario_10_MIXED_SENT.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_10_MIXED-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt03_scenario_10_MIXED_RCVD.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_10_MIXED-*.sca
+
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total SENT Packets")' -o results/VOIP_DSL_rpt10_scenario_10_MIXED_SENT.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_10_MIXED-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] Total RCVD Packets")' -o results/VOIP_DSL_rpt10_scenario_10_MIXED_RCVD.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_10_MIXED-*.sca
 
 #scavetool export  -f 'module(net.server.app[0]) AND name("packetReceived:vector(packetBytes)")' -o results/General-ReceivedBytes.csv -F CSV-S results/General-*.vec

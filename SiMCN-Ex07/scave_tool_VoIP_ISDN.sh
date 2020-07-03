4257fc90e79d844a8640697f2ecc5b8e7d31f0c3
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-07-03 18:16:31 +0200

    updated scripts

--- SiMCN-Ex07/scave_tool_VoIP_ISDN.sh
+++ SiMCN-Ex07/scave_tool_VoIP_ISDN.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20_PK_Loss_Rate.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20_PK_Loss_Rate.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_06_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_06_PAIRS_20-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_07_PAIRS_20_PK_Loss_Rate.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_07_PAIRS_20-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_07_PAIRS_20_PK_Loss_Rate.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_07_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_07_PAIRS.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_07_PAIRS_20-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_07_PAIRS.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_07_PAIRS_20-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300_PK_Loss_Rate.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300_PK_Loss_Rate.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_08_PAIRS_300-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_08_PAIRS_300-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5_PK_Loss_Rate.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5.csv -F CSV-S results/VOIP_ISDN_rpt03_scenario_09_PAIRS_5-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5.csv -F CSV-S results/VOIP_ISDN_rpt10_scenario_09_PAIRS_5-*.sca
 
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_10_MIXED_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_10_MIXED-*.sca
-scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_10_MIXED_PK_Loss_Rate.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_10_MIXED-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt03_scenario_10_MIXED.csv -F CSV-S results/VOIP_DSL_rpt03_scenario_10_MIXED-*.sca
+scavetool export  -f 'module(*.user[*].*) AND name("[VoIP] *")' -o results/VOIP_DSL_rpt10_scenario_10_MIXED.csv -F CSV-S results/VOIP_DSL_rpt10_scenario_10_MIXED-*.sca
 
 #scavetool export  -f 'module(net.server.app[0]) AND name("packetReceived:vector(packetBytes)")' -o results/General-ReceivedBytes.csv -F CSV-S results/General-*.vec

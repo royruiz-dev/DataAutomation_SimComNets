bd0c84fc077d3074691f11308ea5a467185d1058
Sajid Maqbool <sajid.maqbool@tuhh.de>
2020-07-06 22:08:03 +0000

    Delete run_build.sh

--- SiMCN-Ex07/run_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/workspace/a-team-2/SiMCN-Ex07
-opp_makemake -f --deep -O out -KINET_PROJ=${HOME}/workspace/inet4 -DINET_IMPORT -I. -I$\(INET_PROJ\)/src -L$\(INET_PROJ\)/src -lINET$\(D\)
-make MODE=release
-./SiMCN-Ex07 -a

aef609f31921aaa749934991562535f2fa582e40
Sajid Maqbool <sajid.maqbool@tuhh.de>
2020-07-06 22:11:01 +0000

    Delete scave_tool_No_VoIP_ISDN.sh

--- SiMCN-Ex07/scave_tool_No_VoIP_ISDN.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scavetool export  -f 'module(*.user[*].pppg$o[0].channel) AND name(throughput:last)' -o results/NO_VOIP_ISDN_rpt03_scenario_01_throughput.csv -F CSV-S results/NO_VOIP_ISDN_rpt03_scenario_01-*.sca
-scavetool export  -f 'module(*.user[*].pppg$o[0].channel) AND name(throughput:last)' -o results/NO_VOIP_ISDN_rpt10_scenario_01_throughput.csv -F CSV-S results/NO_VOIP_ISDN_rpt10_scenario_01-*.sca
-scavetool export  -f 'module(*.user[*].pppg$o[0].channel) AND name(throughput:last)' -o results/NO_VOIP_ISDN_rpt30_scenario_01_throughput.csv -F CSV-S results/NO_VOIP_ISDN_rpt30_scenario_01-*.sca

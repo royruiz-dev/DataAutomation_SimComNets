361f17e0e7f597d5e67a782cac9481dae35adc85
Sajid Maqbool <sajid.maqbool@tuhh.de>
2020-07-06 22:10:55 +0000

    Delete run_sim_scnr_07.sh

--- SiMCN-Ex07/run_sim_scnr_07.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c VOIP_ISDN_rpt03_scenario_07_PAIRS_300 -r 0..2 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Ex07 -u Cmdenv -c VOIP_ISDN_rpt10_scenario_07_PAIRS_300 -r 0..9 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini

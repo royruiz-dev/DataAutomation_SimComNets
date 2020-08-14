a4e071453073948edfbad6034901224322d2a1ac
Roy Ruiz <roy.ruiz@tuhh.de>
2020-08-14 14:43:43 +0200

    Updated scavetool bash script to add new delay scalar

--- SiMCN-Final-Task/scavetool.sh
+++ SiMCN-Final-Task/scavetool.sh
@@ -28,37 +28,44 @@ scavetool export  -f 'module(*) AND name(packetDrop:count)' -o results/HTTP_Clie
 scavetool export  -f 'module(*) AND name(packetDrop:count)' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
 scavetool export  -f 'module(*) AND name(packetDrop:count)' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
 
-scavetool export  -f 'module(*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] AVG PK LOSS Rate")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
+scavetool export  -f 'module(*) AND name("[VC] AVG PK LOSS Rate")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] AVG PK LOSS Rate")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] AVG PK LOSS Rate")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
+scavetool export  -f 'module(*) AND name("[VC] AVG PK LOSS Rate")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
+scavetool export  -f 'module(*) AND name("[VC] AVG PK LOSS Rate")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
+scavetool export  -f 'module(*) AND name("[VC] AVG PK LOSS Rate")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
 
-scavetool export  -f 'module(*) AND name("[VoIP] Total LATE Packets")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LATE Packets")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LATE Packets")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LATE Packets")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LATE Packets")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LATE Packets")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LATE Packets")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LATE Packets")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LATE Packets")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LATE Packets")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LATE Packets")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LATE Packets")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
 
-scavetool export  -f 'module(*) AND name("[VoIP] Total LOST Packets")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LOST Packets")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LOST Packets")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LOST Packets")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LOST Packets")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total LOST Packets")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LOST Packets")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LOST Packets")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LOST Packets")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LOST Packets")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LOST Packets")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total LOST Packets")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
 
-scavetool export  -f 'module(*) AND name("[VoIP] Total SENT Packets")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total SENT Packets")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total SENT Packets")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total SENT Packets")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total SENT Packets")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total SENT Packets")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total SENT Packets")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total SENT Packets")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total SENT Packets")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total SENT Packets")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total SENT Packets")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total SENT Packets")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
 
-scavetool export  -f 'module(*) AND name("[VoIP] Total RCVD Packets")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total RCVD Packets")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total RCVD Packets")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total RCVD Packets")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total RCVD Packets")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
-scavetool export  -f 'module(*) AND name("[VoIP] Total RCVD Packets")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total RCVD Packets")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total RCVD Packets")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total RCVD Packets")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total RCVD Packets")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total RCVD Packets")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Total RCVD Packets")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca
+
+scavetool export  -f 'module(*) AND name("[VC] Avg End-to-End Delay")' -o results/VC_with_FTP_Only.csv -F CSV-S results/VC_with_FTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Avg End-to-End Delay")' -o results/VC_with_HTTP_Only.csv -F CSV-S results/VC_with_HTTP_Only-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Avg End-to-End Delay")' -o results/VC_with_FTP_and_HTTP.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Avg End-to-End Delay")' -o results/HTTP_Client_Increase_S04a.csv -F CSV-S results/HTTP_Client_Increase_S04a-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Avg End-to-End Delay")' -o results/HTTP_Client_Increase_S04b.csv -F CSV-S results/HTTP_Client_Increase_S04b-*.sca
+scavetool export  -f 'module(*) AND name("[VC] Avg End-to-End Delay")' -o results/HTTP_Client_Decrease_S05.csv -F CSV-S results/HTTP_Client_Decrease_S05-*.sca

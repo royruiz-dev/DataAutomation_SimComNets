303d5f5e75ad9354a6b184112b2c93fb547c5a15
Roy Ruiz <roy.ruiz@tuhh.de>
2020-08-14 14:37:35 +0200

    Updated the ini and bash script files

--- SiMCN-Final-Task/run_sim_scnr_VC_HTTP.sh
+++ SiMCN-Final-Task/run_sim_scnr_VC_HTTP.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c [CONFIG_NAME] -r [REPEAT VALUE AS 0..X] -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src final-task-2020-common-ini-file.ini
 
-opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_FTP_Only_repeat10 -r 0..9 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_FTP_Only_repeat30 -r 0..29 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-
-opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_HTTP_Only_repeat10 -r 0..9 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_HTTP_Only_repeat30 -r 0..29 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-
-opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_FTP_and_HTTP_repeat10 -r 0..9 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_FTP_and_HTTP_repeat30 -r 0..29 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
-
+opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_FTP_Only -r 0..9 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_HTTP_Only -r 0..9 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini
+opp_runall -j$(nproc) ./SiMCN-Final-Task -u Cmdenv -c VC_with_FTP_and_HTTP -r 0..9 -n .:/fibus/fs2/19/cbh2268/workspace/inet4/src omnetpp.ini

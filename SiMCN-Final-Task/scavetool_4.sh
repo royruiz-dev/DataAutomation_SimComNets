c2c143d848265f4e85cef6729da5e46bf7628c63
Roy Ruiz <roy.ruiz@tuhh.de>
2020-08-24 17:45:39 +0200

    New scavetool script to pull vector data

--- SiMCN-Final-Task/scavetool_4.sh
+++ SiMCN-Final-Task/scavetool_4.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-scavetool export  -f 'module(*) AND name("[VC] End-to-End Delay")' -o results/HTTP_Clients_6_E2E_Delay_Vector.csv -F CSV-S results/HTTP_Clients_6-*.vec
-scavetool export  -f 'module(*) AND name("[VC] End-to-End Delay")' -o results/HTTP_Clients_7_E2E_Delay_Vector.csv -F CSV-S results/HTTP_Clients_7-*.vec
+scavetool export  -f 'module(*.Student.*) AND name("[VC] End-to-End Delay")' -o results/HTTP_Clients_6_Student_E2E_Delay_Vector.csv -F CSV-S results/HTTP_Clients_6-*.vec
+scavetool export  -f 'module(*.Professor.*) AND name("[VC] End-to-End Delay")' -o results/HTTP_Clients_6_Professor_E2E_Delay_Vector.csv -F CSV-S results/HTTP_Clients_6-*.vec
+scavetool export  -f 'module(*.Student.*) AND name("[VC] End-to-End Delay")' -o results/HTTP_Clients_7_Student_E2E_Delay_Vector.csv -F CSV-S results/HTTP_Clients_7-*.vec
+scavetool export  -f 'module(*.Professor.*) AND name("[VC] End-to-End Delay")' -o results/HTTP_Clients_7_Professor_E2E_Delay_Vector.csv -F CSV-S results/HTTP_Clients_7-*.vec

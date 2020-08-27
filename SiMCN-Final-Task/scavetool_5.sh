87a501cbf42f6f88439b60e303f2f8c57fb7f15d
Sajid Maqbool <sajid.maqbooltuhh@gmail.com>
2020-08-27 14:52:18 +0200

    AP pending queue

---
+++ SiMCN-Final-Task/scavetool_5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scavetool export  -f 'module(FinalTask.AP.wlan[0].mac.dcf.channelAccess.pendingQueue) AND name(queueLength:max)' -o results/HTTP_Clients_6_AP_PendingQueue.csv -F CSV-S results/HTTP_Clients_6-*.sca
+scavetool export  -f 'module(FinalTask.AP.wlan[0].mac.dcf.channelAccess.pendingQueue) AND name(queueLength:max)' -o results/HTTP_Clients_3_AP_PendingQueue.csv -F CSV-S results/HTTP_Clients_3-*.sca
+scavetool export  -f 'module(FinalTask.AP.wlan[0].mac.dcf.channelAccess.pendingQueue) AND name(queueLength:max)' -o results/HTTP_Clients_10_AP_PendingQueue.csv -F CSV-S results/HTTP_Clients_10-*.sca
+scavetool export  -f 'module(FinalTask.AP.wlan[0].mac.dcf.channelAccess.pendingQueue) AND name(queueLength:max)' -o results/HTTP_Clients_7_AP_PendingQueue.csv -F CSV-S results/HTTP_Clients_7-*.sca

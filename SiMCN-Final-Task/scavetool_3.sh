591ea4a389b6d6a994532c26983808f342020f72
Roy Ruiz <roy.ruiz@tuhh.de>
2020-08-27 17:22:04 +0200

    Minor change in the scavetool script

--- SiMCN-Final-Task/scavetool_3.sh
+++ SiMCN-Final-Task/scavetool_3.sh
@@ -3,6 +3,7 @@
 scavetool export  -f '(module(*.AP.*ueue) AND name(queueLength:max)) OR (module(*Router.*) AND name(queueLength:max))' -o results/HTTP_Clients_1_queueLength_max.csv -F CSV-S results/HTTP_Clients_1-*.sca
 scavetool export  -f '(module(*.AP.*ueue) AND name(queueLength:max)) OR (module(*Router.*) AND name(queueLength:max))' -o results/HTTP_Clients_2_queueLength_max.csv -F CSV-S results/HTTP_Clients_2-*.sca
 scavetool export  -f '(module(*.AP.*ueue) AND name(queueLength:max)) OR (module(*Router.*) AND name(queueLength:max))' -o results/HTTP_Clients_3_queueLength_max.csv -F CSV-S results/HTTP_Clients_3-*.sca
+scavetool export  -f '(module(*.AP.*ueue) AND name(queueLength:max)) OR (module(*Router.*) AND name(queueLength:max))' -o results/HTTP_Clients_4_queueLength_max.csv -F CSV-S results/VC_with_FTP_and_HTTP-*.sca
 scavetool export  -f '(module(*.AP.*ueue) AND name(queueLength:max)) OR (module(*Router.*) AND name(queueLength:max))' -o results/HTTP_Clients_5_queueLength_max.csv -F CSV-S results/HTTP_Clients_5-*.sca
 scavetool export  -f '(module(*.AP.*ueue) AND name(queueLength:max)) OR (module(*Router.*) AND name(queueLength:max))' -o results/HTTP_Clients_6_queueLength_max.csv -F CSV-S results/HTTP_Clients_6-*.sca
 scavetool export  -f '(module(*.AP.*ueue) AND name(queueLength:max)) OR (module(*Router.*) AND name(queueLength:max))' -o results/HTTP_Clients_7_queueLength_max.csv -F CSV-S results/HTTP_Clients_7-*.sca
